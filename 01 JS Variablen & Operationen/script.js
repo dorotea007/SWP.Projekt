af4eaea5503eb738f138eca1efd6a6b6ff8f78aa
dorotea007 <Dorotea.Milic@student.htldornbirn.at>
2023-03-07 18:27:34 +0100

    Aufgabe 01

---
+++ 01 JS Variablen & Operationen/script.js
@@ -0,0 +1,45 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+// Berechne folgendes und gib das Eregebnis über console.log aus
+
+let result = a + b + c;
+console.log("Das Ergebnis ist " + result);
+// a + b + c
+
+result = (a + b) / c;
+console.log("Das Ergebnis ist " + result);
+// (a + b) / c
+
+result = c - a + b;
+console.log("Das Ergebnis ist " + result);
+// c -a + b
+
+result = a + b + c * 3;
+console.log("Das Ergebnis ist " + result);
+// a + b + c * 3
+
+result = (a + b + c) / 4;
+console.log("Das Ergebnis ist " + result);
+// (a + b + c)/4
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+// b = a + 3
+// c = c -3
+// gib die neuen Werte der Variablen aus
+console.log("a " + a);
+a = 3;
+console.log("a = 3 " + a);
+
+console.log("b " + b);
+b = a + 3;
+console.log("b = a + 3 " + b);
+
+console.log("c " + c);
+c = c - 3;
+console.log("c = c - 3 " + c);
