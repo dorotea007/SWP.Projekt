0638e4e949649f78b879314b078752d23c5388e3
dorotea007 <Dorotea.Milic@student.htldornbirn.at>
2023-03-08 08:23:53 +0100

    Aufgabe 02

---
+++ 02 JS Strings & Zahlen/script.js
@@ -0,0 +1,37 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+
+// +, -, *, /
+
+let c = 7.4;
+let d = 7.5;
+
+result = c + d;
+console.log(result);
+
+// Texte STRINGS
+
+let name = "Andreas";
+console.log(name);
+
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + ab);
+
+//	1) Berechne das Ergebnis von 17.4 / 3.9. Gib folgenden Satz aus: 
+//The result of 17.4 divided by 3.9 is 4.461538461538462 
+
+let e = 17.4;
+let f = 3.9;
+
+let ef = e / f;
+
+console.log("The result of " + e + "divided by " + f + "is " + ef);
+
+// 2) Lege eine Variable firstname und eine Varialbe lastname an. Weise den Variablen Deinen Namen zu. Gib folgendes aus:
+//"Ich heiße VORNAME NACHNAME"
+
+let firstname = "Dorotea";
+let lastname = "Milic";
+
+console.log("Ich heiße "  + firstname + " " + lastname);
