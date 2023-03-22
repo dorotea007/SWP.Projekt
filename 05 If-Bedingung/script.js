5e2b1e99fc2b06e31cb2b16b5b879f10aae49b84
dorotea007 <Dorotea.Milic@student.htldornbirn.at>
2023-03-22 09:01:13 +0100

    Aufgabe 05 If_Bedingung

---
+++ 05 If-Bedingung/script.js
@@ -0,0 +1,36 @@
+
+// erstelle eine Variable number
+let number = 10;
+
+// Aufgabe 1
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+if (number < 20) {
+    console.log("Mini");
+}
+
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+else if (number >= 20 && number <= 50) {
+    console.log("Medium");
+
+}
+
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+else {
+    console.log("Large");
+}
+
+// Aufgabe 2
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+if (number <= 7) {
+    console.log("Mini");
+} 
+
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+else if(number > 7 && number <= 90) {
+    console.log("Medium");
+}
+
+// Wenn die Zahl größer als 90 ist gib aus "Large"
+else {
+    console.log("Large");
+}
